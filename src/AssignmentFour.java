ce662784d50a4e1ffe37572200a03ffd7704e203
Tyypos <tyyposofficial@gmail.com>
2019-03-27 06:53:04 -0700

    jared: more groundwork

--- src/AssignmentFour.java
+++ src/AssignmentFour.java
@@ -10,7 +10,82 @@ public class AssignmentFour
    public static void main(String[] args)
    {
       System.out.println("Hello, AssignmentFour");
-
    }
 
 }
+
+interface BarcodeIO
+{
+   public boolean scan(BarcodeImage bc);
+   public boolean readText(String text);
+   public boolean generateImageFromText();
+   public boolean translateImageToText();
+   public void displayTextToConsole();
+   public void displayImageToConsole();
+}
+
+class BarcodeImage implements Cloneable
+{
+   public static final int MAX_HEIGHT = 30;
+   public static final int MAX_WIDTH = 65;
+   private boolean[][] imageData;
+}
+
+class DataMatrix implements BarcodeIO
+{
+   public static final char BLACK_CHAR = '*';
+   public static final char WHITE_CHAR = ' ';
+   private BarcodeImage image;
+   private String text;
+   private int actualWidth;
+   private int actualHeight;
+
+   public boolean scan(BarcodeImage bc)
+   {
+      return true;
+   }
+   
+   public boolean readText(String text)
+   {
+      return true;
+   }
+   
+   public boolean generateImageFromText()
+   {
+      return true;
+   }
+   
+   public boolean translateImageToText()
+   {
+      return true;
+   }
+   
+   public void displayTextToConsole()
+   {
+      
+   }
+   
+   public void displayImageToConsole()
+   {
+      
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
