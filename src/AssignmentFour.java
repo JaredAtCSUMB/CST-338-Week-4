79953fa4288ec6a8c78a5ca2d743459d05410884
Tyypos <tyyposofficial@gmail.com>
2019-03-27 06:23:22 -0700

    jared: initial project set up

---
+++ src/AssignmentFour.java
@@ -0,0 +1,16 @@
+/**
+ * This application simulates a barcode scanner...
+ * 
+ * @author Team 6: Jared Cheney, Andrew Meraz, Chul Kim and Agustin Garcia
+ *
+ */
+public class AssignmentFour
+{
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello, AssignmentFour");
+
+   }
+
+}
